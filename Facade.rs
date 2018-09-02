d2d84d7f1db23fa3a5a6d3da25cbc4d0926f20d1
Aykull <31603222+Aykull@users.noreply.github.com>
2018-09-02 16:30:05 -0600

    Add files via upload

--- Facade.rs
+++ Facade.rs
@@ -1,9 +1,36 @@
-struct Processor;
-    fn model(){
-        println!("This is an Intel core i5");
+pub struct FactoryFacade {
+    let mut engine = Engine::new();
+    let mut body = Body::new();
+    let mut accessories = Accessories::new();
+
+    fn produceCar() {
+        engine.produceEngine()
+        body.produceBody()
+        accessories.produceAccessories()
+    }
+}
+
+pub trait Engine {
+    fn produceEngine() {
+        print!("prodce engine");
+    }
+}
+
+pub trait Body {
+    fn produceBody() {
+        print!("prodce body");
+    }
+}
+
+pub trait Accessories {
+    fn produceAccessories() {
+        print!("prodce accessories");
+    }
+}
+
+
+fn main() {
+    let mut factoryFacade = FactoryFacade::new();
+    factoryFacade.produceCar();
 
 }
-struct RAM;
-    fn capacity(){
-        println!("This ram is GB");
-    }
