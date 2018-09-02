085177031e30625a454080e9f8b1cccf56b8fbd4
DantrollCR <daniel-2443@hotmail.com>
2018-09-02 08:52:51 -0600

    Abstract que se puede agragar al menu
    Agregar esto en el main:
    FabricaCarros* getFActory(const string FactoryID)
    {
        if(FactoryID == "Lambo")
            return new LamboFactory();
        else
            return new SUVFactory();
    }
    int main()
    {
    
        Application application(getFActory("Lambo"));
        return 0;
    }

---
+++ AbstractF.cpp
@@ -0,0 +1,85 @@
+//
+// Created by dantroll on 9/2/18.
+//
+#include "AbstractF.h"
+#include <iostream>
+
+using namespace std;
+// Abstract product
+class Carro
+{
+public:
+    virtual void readModel()=0;
+    virtual void readYear()=0;
+};
+//Se hace una clase para un carro específico, un Lamborguini
+ class Lambo:public Carro
+{
+public:
+    Lambo() {}
+    void readModel()
+    {
+        cout << "Soy un Lamborguini Diablo"<<endl;
+    }
+    void readYear()
+    {
+        cout << "Y soy año 2018"<<endl;
+    }
+};
+
+//Una clases para un auto SUV
+class SUV:public Carro
+{
+public:
+    SUV() {}
+    void readModel()
+    {
+        cout << "Soy un LandRover Dicovery"<<endl;
+    }
+    void readYear()
+    {
+        cout << " Soy año 2003"<<endl;
+    }
+};
+
+// Abstract Factory de Carros
+class FabricaCarros
+{
+public:
+    virtual Carro* getProduct()=0;
+};
+// Fabrica de un Lmborguini
+class LamboFactory:public FabricaCarros
+{
+public:
+    Carro* getProduct()
+    {
+        return new Lambo();
+    }
+    ~LamboFactory(){}
+};
+// Fabrica de un SUV
+class SUVFactory:public FabricaCarros
+{
+public:
+    Carro* getProduct()
+    {
+        return new SUV();
+    }
+    ~SUVFactory(){}
+};
+
+
+
+class Application
+{
+public:
+    Application(FabricaCarros* factory)
+    {
+        Carro* product = factory->getProduct();
+        product->readModel();
+        product->readYear();
+        delete product;
+        delete factory;
+    }
+};
